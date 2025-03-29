f5682e396bc14b67143a3262ff765b9fe3c6407f
Veerendra Gupta <veerendragupta52525@gmail.com>
2025-03-30 00:03:13 +0530

    Write tests for single number input

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe StringCalculator do
       expect(StringCalculator.add("")).to eq(0)
     end
   end
+
+  it "returns the number for a single number" do
+    expect(StringCalculator.add("1")).to eq(1)
+  end
 end
